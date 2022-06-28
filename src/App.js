05d3308e89f8af0f7f28227c9dc7f7d4bf391f5e
Anurag <anurag.yadav@mphasis.com>
2022-06-28 22:53:23 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './Sass/main.css'
+import React, { Component } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
+import Links from './Links/Links';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Services from './Pages/Services';
+import { pageDetails } from './Mock/Mock';
+
+
+class App extends Component{
+  render() {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Links/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+
+
+
+        {/* {
+          pageDetails.map(({name, path})=>{
+            
+            return (
+              <>
+              <Route path={path} element={name} />
+              </>
+            )
+          })
+        } */}
+      </Routes>
+      </>
+
+
+
+
+
+
+
+
+        {/* <nav className='navBar'>
+          <img src="../images/mphasisLogo.png" alt="not found" />
+          <ul>
+            <li>
+              <Link className='links' to='/'>Home</Link>
+            </li>
+            <li>
+              <Link className='links' to='about'>About</Link>
+            </li>
+            <li>
+              <Link className='links' to='services'>Services</Link>
+            </li>
+          </ul>
+        </nav> */}
+     
+
     </div>
-  );
+    )
+  };
 }
 
 export default App;
